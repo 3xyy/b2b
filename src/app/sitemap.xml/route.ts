1ee21177013a29d01913fbd700be28420e2aec0e
vk0xcipher <vk0xcipher@gmail.com>
2026-02-25 18:06:35 -0800

    fix: use force-static for sitemap.xml route in static export

--- src/app/sitemap.xml/route.ts
+++ src/app/sitemap.xml/route.ts
@@ -1,4 +1,4 @@
-export const revalidate = 3600;
+export const dynamic = "force-static";
 
 export async function GET() {
   const baseUrl = 'https://FreeForCharity.github.io/FFC-EX-bintobetter.org';
